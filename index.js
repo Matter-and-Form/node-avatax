f370f9a730df848ea29e6e2e064e65f23feb7bf4
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2014-06-24 18:48:49 -0400

    initial commit
    0.0.1
    
    rename; added estimation feature; start of get tax;
    
    0.0.2
    
    getTax added; respective tests
    
    0.0.3
    
    Better error handling; fix major issue with request options being reused and defaults overwritten
    
    0.0.4
    
    remove unecessary console.log
    
    0.0.5
    
    moved authentication to non-tracked file

---
+++ index.js
@@ -0,0 +1,326 @@
+"use strict";
+
+var https = require('https');
+var url = require('url');
+
+var latestAPIVersion = "1.0";
+
+/*
+===========
+Constructor
+===========
+*/
+
+function AvaTax(username, password, options) {
+
+	if (!username || typeof username !== "string" || !password || typeof password !== "string") {
+		throw new Error("Credentials not supplied for AvaTax");
+	}
+
+	this.username = username;
+	this.password = password;
+
+	options = options || {};
+	options.development = options.development || false;//Development or Production
+	options.version = options.version || latestAPIVersion;
+	options.client = typeof options.client != "undefined"? options.client : "node-avatax";
+	options.client = options.client === false ? undefined : options.client;
+
+	if (options.version > latestAPIVersion) {
+		throw new Error("This library does not support an API version greater than `" + latestAPIVersion + "`. Contact this module's author to allow for newer versions.");
+	}
+
+	var authenticationHeader = "Basic " + new Buffer([this.username,this.password].join(":"), 'utf8').toString('base64');
+
+	this.requestOptions = function() {
+		var requestOptions = {
+			port: 443,//https
+			method: 'GET',
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json",
+				"Authorization": authenticationHeader
+			}
+		};
+
+		requestOptions.host = options.development ? 'development.avalara.net' : 'avatax.avalara.net';
+		requestOptions.host = options.host || requestOptions.host;//allow overriding of host completely
+		return requestOptions;
+	};
+	this.options = options;
+
+	return this;
+}
+
+
+/*
+=========
+Utilities
+=========
+*/
+
+
+function cloneObject(object) {
+	var clone = {};
+	for (var key in object) {
+		if (object.hasOwnProperty(key)) {
+			clone[key] = object[key];
+		}
+	}
+	return clone;
+}
+
+AvaTax.prototype._makeRequest = function(requestOptions, requestBody, next) {
+
+	var responseBody = "";
+
+	var req = https.request(requestOptions, function(res) {
+
+		res.setEncoding('utf-8');
+		res.on('data', function(chunk) {
+			responseBody += chunk;
+		});
+
+		res.on('end', function() {
+
+			var errorText = "AvaTax server error";
+			var error;
+			if (/^4\d\d/.test(res.statusCode)) {
+				error = new Error(errorText);
+				error.message = errorText;
+				error.code = res.statusCode;
+				return next(error);
+			}
+
+			var json = JSON.parse(responseBody);
+
+			if (res.statusCode === 500) {
+				if (json.Messages && json.Messages.length && json.Messages[0].Summary) {
+					errorText = json.Messages[0].Summary;
+				}
+				error = new Error(errorText);
+				error.message = errorText;
+				error.code = 500;
+				return next(error);
+			}
+
+			next(null, json);
+		});
+	});
+
+	req.on('error', function(err) {
+		console.log('http req error: ' + err.message || err);
+		return next(err);
+	});
+
+	req.write(requestBody);
+	req.end();//end request, proceed to response
+};
+
+/*
+========
+Validate Address
+========
+*/
+
+AvaTax.prototype._validateAddress = function(options, next) {
+
+	options = options || {};
+	var requestBody = "";
+
+	//var requestBody = AvaTax.prototype._newRequestBody.call(this, requestBodyOptions);
+
+	var addressObject = {
+		Line1: options.Line1,
+		Line2: options.Line2,
+		Line3: options.Line3,
+		City: options.City,
+		Region: options.Region,
+		Country: options.Country,
+		PostalCode: options.PostalCode
+	};
+
+	if (!(typeof addressObject.Line1 != "undefined" && typeof addressObject.PostalCode != "undefined") || !(typeof addressObject.Line1 != "undefined" && typeof addressObject.City != "undefined" && typeof addressObject.Region != "undefined")) {
+		return next(new Error("You must specify at least Line & Postal Code, or Line & City & Region"));
+	}
+
+	var requestOptions = this.requestOptions();
+	requestOptions.path = url.format({
+		pathname: "/" + this.options.version + '/address/validate',
+		query: addressObject
+	});
+
+	this._makeRequest(requestOptions, requestBody, function(err, json) {
+		if (err) {
+			return next(err);
+		}
+
+		if (json.ResultCode == "Error") {
+			var error = new Error(json.Messages[0].Summary);
+			return next(error);
+		}
+
+		if (json.ResultCode == "Success") {
+			return next(null, json.Address);
+		}
+
+		return next(null, json);
+	});
+
+};
+
+AvaTax.prototype.validateAddress = function(address, next) {
+	if (!next) {//if typeof options is a function?
+		next = address;
+		address = {};
+	}
+
+	return AvaTax.prototype._validateAddress.call(this, address, next);
+};
+
+
+/*
+========
+Tax
+========
+*/
+
+AvaTax.prototype._estimateTax = function(options, next) {
+
+	options = options || {};
+
+	var requestBody = "";
+	//requestBody = AvaTax.prototype._newRequestBody.call(this, requestBodyOptions);
+
+	var requestOptions = this.requestOptions();
+	requestOptions.path = url.format({
+		pathname: "/" + this.options.version + '/tax/' + options.latitude + "," + options.longitude +
+		"/get",
+		query: {
+			saleamount: options.amount
+		}
+	});
+
+	//requestOptions.method = "POST";
+
+	this._makeRequest(requestOptions, requestBody, function(err, json) {
+		if (err) {
+			return next(err);
+		}
+
+		return next(null, json);
+	});
+
+};
+
+AvaTax.prototype.estimateTax = function(coordinates, amount, next) {
+
+	var options = {
+		latitude: coordinates[0],
+		longitude: coordinates[1],
+		amount: amount
+	};
+
+	return AvaTax.prototype._estimateTax.call(this, options, function(err, json) {
+		if (err) {
+			return next(err);
+		}
+
+		next(null, json.Tax);
+	});
+};
+
+AvaTax.prototype.estimateTaxDetails = function(coordinates, amount, next) {
+
+	var options = {
+		latitude: coordinates[0],
+		longitude: coordinates[1],
+		amount: amount
+	};
+
+	return AvaTax.prototype._estimateTax.call(this, options, function(err, json) {
+		if (err) {
+			return next(err);
+		}
+
+		next(null, json.TaxDetails);
+	});
+};
+
+
+AvaTax.prototype._getTax = function(options, next) {
+
+	options = options || {};
+
+	var requestBody = JSON.stringify(options, null, '\t');
+	//requestBody = AvaTax.prototype._newRequestBody.call(this, requestBodyOptions);
+
+	var requestOptions = this.requestOptions();
+	requestOptions.path = url.format({
+		pathname: "/" + this.options.version + '/tax/get'
+	});
+	requestOptions.method = "POST";
+
+	this._makeRequest(requestOptions, requestBody, function(err, json) {
+		if (err) {
+			return next(err);
+		}
+
+		return next(null, json);
+	});
+
+};
+
+AvaTax.prototype.getTax = function(fields, next) {
+
+	var i = 0;
+	var options = {
+		CustomerCode: fields.CustomerCode,
+		DocDate: fields.DocDate || new Date(),
+		CompanyCode: fields.CompanyCode,
+		Commit: typeof fields.Commit != "undefined"? fields.Commit : false,
+		CurrencyCode: fields.CurrencyCode || "USD",
+		CustomerUsageType: fields.CustomerUsageType,
+		Discount: fields.Discount,
+		DocCode: fields.DocCode,
+		PurchaseOrderNo: fields.PurchaseOrderNo,
+		ExemptionNo: fields.ExemptionNo,
+		DetailLevel: fields.DetailLevel,
+		DocType: fields.DocType,
+		Lines: fields.Lines,
+		Addresses: fields.Addresses,
+		ReferenceCode: fields.ReferenceCode,
+		PosLaneCode: fields.PosLaneCode,
+		Client: typeof fields.Client != "undefined"? fields.Client : this.options.client,
+		TaxOverride: fields.TaxOverride,
+		BusinessIdentificationNo: fields.BusinessIdentificationNo
+	};
+
+	options.DocDate = [
+		options.DocDate.getFullYear(),
+		options.DocDate.getMonth()+1 < 10? "0" + (options.DocDate.getMonth()+1) : (options.DocDate.getMonth()+1),
+		options.DocDate.getDate() < 10? "0" + options.DocDate.getDate() : options.DocDate.getDate()
+	].join("-");
+
+	if (typeof options.CustomerCode == "undefined" || typeof options.Lines == "undefined" || !Array.isArray(options.Lines) || !options.Lines.length || typeof options.Addresses == "undefined" || !Array.isArray(options.Addresses) || !options.Addresses.length) {
+		return next(new Error("Missing required fields"));
+	}
+
+	for (i = 0; i < options.Lines.length; i++) {
+		var line = options.Lines[i];
+		if (typeof line.LineNo == "undefined" || typeof line.DestinationCode == "undefined" || typeof line.OriginCode == "undefined" || typeof line.Qty == "undefined" || typeof line.Amount == "undefined") {
+			return next(new Error("Line indexOf#" + i + " is missing required fields"));
+		}
+	}
+
+	for (i = 0; i < options.Addresses.length; i++) {
+		var address = options.Addresses[i];
+		if (typeof address.AddressCode == "undefined" || (((typeof address.Line1 == "undefined" || typeof address.City == "undefined") || (typeof address.Line1 == "undefined" || typeof address.PostalCode == "undefined")) && (typeof address.Latitude == "undefined" || typeof address.Longitude == "undefined"))) {
+			return next(new Error("Address indexOf#" + i + " is missing required fields"));
+		}
+	}
+
+	return AvaTax.prototype._getTax.call(this, options, next);
+};
+
+module.exports = AvaTax;
---
+++ test/test.js
@@ -0,0 +1,222 @@
+"use strict";
+
+var assert = require("assert");
+var fs = require('fs');
+
+var authentication = require("./authentication.json");
+var username = authentication.username;
+var password = authentication.password;
+
+var AvaTax = require("../");//avatax;
+var avatax = new AvaTax(username, password, {
+	development: true
+});
+
+var maxTimeout = 1e4;//10 seconds
+var slowTime = 1e3;//1 second
+
+var address = {
+	AddressCode: "destination",
+	Line1: "118 N Clark St",
+	Line2: "Suite 100",
+	Line3: "ATTN Accounts Payable",
+	City: "Chicago",
+	Region: "IL",
+	PostalCode: "60602",
+	Country: "US"
+};
+
+var address2 = {
+	AddressCode: "origin",
+	Line1: "151 Sterling Rd.",
+	Line2: "Studio 2",
+	City: "Toronto",
+	Region: "Ontario",
+	Country: "Canada",
+	PostalCode: "M6R 2B2"
+};
+
+var address3 = {
+	AddressCode: "destination",
+	Line1: "1 Infinite Loop",
+	Line2: "ATTN Tim Cook",
+	City: "Cupertino",
+	Region: "CA",
+	PostalCode: "95014",
+	Country: "US"
+};
+
+var address4 = {
+	AddressCode: "destination",
+	Line1: "1 Churchill Place",
+	Line2: "Barclays Bank PLC",
+	City: "London",
+	//Region: "London",
+	PostalCode: "E145HP",
+	Country: "UK"
+};
+
+function newGetTaxObject() {
+	var object = {
+		CustomerCode: "101",
+		DocDate: new Date("2014-05-13T05:05:30.036Z"),
+		//CompanyCode: fields.CompanyCode,
+		Commit: false,
+		CurrencyCode: "CAD",
+		//CustomerUsageType: fields.CustomerUsageType,
+		//Discount: fields.Discount,
+		//DocCode: fields.DocCode,
+		PurchaseOrderNo: "ABC99",
+		//ExemptionNo: fields.ExemptionNo,
+		//DetailLevel: fields.DetailLevel,
+		DocType: "SalesOrder",
+		Lines: [
+			{
+				LineNo: "1",
+				DestinationCode : "destination",
+				OriginCode: "origin",
+				ItemCode: "MFS1V1",
+				Qty: 2,
+				Amount: 1158.99,
+				Discounted: false
+			}
+		],
+		Addresses: [address3, address2],
+		Client: "node-avatax",
+	};
+	return object;
+}
+
+var coordinates = [47.627935,-122.51702];
+
+describe('AvaTax', function() {
+
+	describe("New AvaTax instance", function() {
+		it('should throw an error when username and/or passwords are missing', function() {
+			assert.throws(function() {
+				new AvaTax();
+			}, Error);
+
+			assert.throws(function() {
+				new AvaTax({
+					development: false,
+				});
+			}, Error);
+
+			assert.throws(function() {
+				new AvaTax("username");
+			}, Error);
+
+			assert.throws(function() {
+				new AvaTax("username", {
+					development: true
+				});
+			}, Error);
+		});
+	});
+
+	describe('#validateAddress()', function() {
+		this.timeout(maxTimeout);
+		this.slow(slowTime);
+
+		it('should return a json response', function(done) {
+			avatax.validateAddress(address3, function(err, address) {
+				console.log(err, address);
+				assert.equal(err, null);
+				assert.equal(true, !!address);
+				//assert.equal(true, !!address.line1);
+				done();
+			});
+		});
+
+		//currently can't find these addresses and need to figure out error handling;
+		/*it('should return a json response when only providing street and postal code', function(done) {
+			avatax.validateAddress({
+				street1: address.street1,
+				street2: address.street2,
+				postalCode: address.postalCode
+			}, function(err, address) {
+				console.log(err, address);
+				assert.equal(err, null);
+				assert.equal(true, !!address);
+				//assert.equal(true, !!address.line1);
+				done();
+			});
+		});
+
+		it('should return a json response when only providing street, city, province', function(done) {
+			avatax.validateAddress({
+				street1: address.street1,
+				street2: address.street2,
+				city: address.city,
+				province: address.province
+			}, function(err, address) {
+				console.log(err, address);
+				assert.equal(err, null);
+				assert.equal(true, !!address);
+				//assert.equal(true, !!address.line1);
+				done();
+			});
+		});*/
+
+	});
+
+	describe('#estimateTax()', function() {
+		this.timeout(maxTimeout);
+		this.slow(slowTime);
+
+		it('should return a number', function(done) {
+			avatax.estimateTax(coordinates, 310.12, function(err, estimate) {
+				console.log(err, estimate);
+				assert.equal(err, null);
+				assert.equal(true, typeof estimate == "number");
+				//assert.equal(true, !!address.line1);
+				done();
+			});
+		});
+	});
+
+	describe('#estimateTaxDetails()', function() {
+		this.timeout(maxTimeout);
+		this.slow(slowTime);
+
+		it('should return an array', function(done) {
+			avatax.estimateTaxDetails(coordinates, 310.12, function(err, estimateDetails) {
+				console.log(err, estimateDetails);
+				assert.equal(err, null);
+				assert.equal(true, Array.isArray(estimateDetails));
+				//assert.equal(true, !!address.line1);
+				done();
+			});
+		});
+	});
+
+	describe('#getTax()', function() {
+		this.timeout(maxTimeout);
+		this.slow(slowTime);
+
+		it('should return an object', function(done) {
+			var getTaxObject = newGetTaxObject();
+			avatax.getTax(getTaxObject, function(err, returnDoc) {
+				console.log(err, returnDoc);
+				assert.equal(err, null);
+				assert.equal(true, !!returnDoc);
+				//assert.equal(true, !!address.line1);
+				done();
+			});
+		});
+
+		it('should throw an error', function(done) {
+			var getTaxObject = newGetTaxObject();
+			getTaxObject.Addresses = [];//remove address, causing a requirements error;
+			avatax.getTax(getTaxObject, function(err, returnDoc) {
+				console.log(err, returnDoc);
+				assert.equal(true, !!err);
+				assert.equal(undefined, returnDoc);
+				//assert.equal(true, !!address.line1);
+				done();
+			});
+		});
+	});
+
+});
